b9314d28ccb21d7eb2ea64ed12433ca36e73768c
Ville Vainio <ville.vainio@aalto.fi>
2017-06-21 18:07:06 +0300

    Do not open unnecessary tabs

--- jisho-anki-bridge/background.js
+++ jisho-anki-bridge/background.js
@@ -1,5 +1,4 @@
 let result;
-
 chrome.runtime.onMessage.addListener(
   (req, sender, res) => {
     console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
@@ -8,7 +7,22 @@ chrome.runtime.onMessage.addListener(
         res({status: "Ok!"})
         let url = 'https://ankiweb.net/edit/'
         result = req.card
-        chrome.tabs.create({url: url})
+        chrome.tabs.query({}, (tabs) => {
+          const re = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im
+          let found = -1
+          for(let i = 0; i < tabs.length; ++i) {
+            let info = tabs[i].url.match(re)
+            if(info[1] === 'ankiweb.net'){
+              found = tabs[i].id
+              break
+            }
+          }
+          if(found != -1) {
+            chrome.tabs.update(found, {active: true, url: url})
+          } else {
+            chrome.tabs.create({url: url})
+          }
+        })
         break
       case 'anki_req':
         res({card: result})
